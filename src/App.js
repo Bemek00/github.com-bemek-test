b820a1f0543621a196d9bf5e49e2670340d2f79c
Bemek00 <patrykbohm00@gmail.com>
2020-04-12 12:29:56 +0200

    change

--- src/App.js
+++ src/App.js
@@ -2,25 +2,51 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const userList = ["Patryk", "Ania", "Amadeusz", "Mateusz", "Patrycja", "Adrianna"];
+
+const ListaUzytkownikow = (props) => {
+  if(props.users.length > 0){
+    return(
+      <ul className="app-ul"> 
+        {props.users.map((user) => <li key={user}> {user} </li>)}
+      </ul>
+    );
+  }
+  else{
+    return(
+      <p className="app-p"> Brak wyników </p>
+    );
+  }
+}
+
+class App extends React.Component{
+  constructor(props){
+    super(props);
+
+    this.state = {filtrowaniUzytkownicy: userList};
+  }
+
+  render(){
+    return(
+      <div className="app">
+        <input type="text" className="app-input" onInput={(e) => this.filtrowanie(e)} />
+        <ListaUzytkownikow users={this.state.filtrowaniUzytkownicy} />
+      </div>
+    );
+  }
+
+  filtrowanie(e){
+    const text = e.target.value;
+    const filtrowaniUzytkownicy = this.filtrowaniUzytkownicyPrzezTekst(text);
+                    
+    this.setState({
+      filtrowaniUzytkownicy
+    });
+  }
+
+  filtrowaniUzytkownicyPrzezTekst(text){
+    return userList.filter((user) => user.toLowerCase().includes(text.toLowerCase()));
+  }
 }
 
 export default App;
