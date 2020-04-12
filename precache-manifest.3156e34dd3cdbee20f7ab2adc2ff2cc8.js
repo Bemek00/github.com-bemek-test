3155d32a7b8b69d867eec19b53ac4720f11e7a55
Bemek00 <patrykbohm00@gmail.com>
2020-04-12 12:02:15 +0200

    Updates

---
+++ precache-manifest.3156e34dd3cdbee20f7ab2adc2ff2cc8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17fbe6a8cc710ec8b982d69cd398a770",
+    "url": "/test/index.html"
+  },
+  {
+    "revision": "b7d93a001574ff4e20e3",
+    "url": "/test/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b4852f586151532f1ead",
+    "url": "/test/static/js/2.7f3bcdb6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/test/static/js/2.7f3bcdb6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7d93a001574ff4e20e3",
+    "url": "/test/static/js/main.ed030bb9.chunk.js"
+  },
+  {
+    "revision": "dbee95b35848d10938ed",
+    "url": "/test/static/js/runtime-main.e4fd221c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test/static/media/logo.5d5d9eef.svg"
+  }
+]);
